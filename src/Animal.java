8594edc0e52aec464215986a18e393ef97ce3cfe
michalbukalski <michalbukalski@gmail.com>
2025-07-12 20:08:20 +0200

    added new interfaces

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,72 @@
+
+enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION, LANDING, LANDED;
+
+    @Override
+    public void track() {
+
+        if (this == GROUNDED) {
+            System.out.println(this + " is grounded and not in flight.");
+        } else if (this == LAUNCH) {
+            System.out.println(this + " is in the launch phase.");
+        } else if (this == CRUISE) {
+            System.out.println(this + " is cruising at altitude.");
+        } else if (this == DATA_COLLECTION) {
+            System.out.println(this + " is collecting data during flight.");
+        } else if (this == LANDING) {
+            System.out.println(this + " is preparing to land.");
+        } else if (this == LANDED) {
+            System.out.println(this + " has landed successfully.");
+        }
+    }
+}
+
+
+
+record DragonFly(String name, String type) implements FlightEnabled {
+
+    @Override
+    public void takeOff() {
+        System.out.println(name + " is taking off");
+    }
+
+    @Override
+    public void land() {
+        System.out.println(name + " is landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(name + " is flying");
+    }
+}
+
+class Satellite implements OrbitEarth {
+
+    public void achieveOrbit() {
+        System.out.println("Orbit achieved around the Earth");
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled {
+
+    void achieveOrbit();
+}
+
 interface FlightEnabled {
 
     double MILES_TO_KM = 1.60934;
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -22,6 +25,29 @@ public class Main {
         truck.track();
         System.out.println("-----------------------");
         inTheRoad(new Truck());
+
+        double kmsTraveled = 100;
+        double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
+        System.out.printf("The truck traveled %.2f km or %.2f miles%n",
+                kmsTraveled, milesTraveled);
+
+        System.out.println("-----------------------");
+
+        LinkedList<FlightEnabled> fliers = new LinkedList<>();
+        fliers.add(bird);
+
+        List<FlightEnabled> betterFliers = new LinkedList<>();
+        betterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        System.out.println("-----------------------");
+
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -39,5 +65,23 @@ public class Main {
             truck.unloadCargo();
         }
         tracked.track();
+    }
+
+    private static void triggerFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.takeOff();
+        }
+    }
+
+    private static void flyFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.fly();
+        }
+    }
+
+    private static void landFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.land();
+        }
     }
 }
